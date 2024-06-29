c92186eb4c0ea35e163c1a470c9fedf2bdf6f73b
PaoloBaltazar <gpaolobaltazar@gmail.com>
2024-06-29 10:27:07 +0800

    Learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -107,3 +107,15 @@ export function displayAddtoCart(productId) {
 
       addedMessageTimeouts[productId] = timeoutId;
 }
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response);
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+}
