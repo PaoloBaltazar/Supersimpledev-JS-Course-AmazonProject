2cb1e4e2eb9abde2fc6b2939c06d404cf6ed4da3
PaoloBaltazar <gpaolobaltazar@gmail.com>
2024-06-29 10:37:26 +0800

    Convert loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -86,6 +86,29 @@ class Clothing extends Product{
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json(); //This returns a promise that needs to be load first before proceeding to the next step
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+  });  
+
+  return promise;
+}
+
+// loadProductsFetch().then(() => {
+//   console.log('next step');
+// })
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
