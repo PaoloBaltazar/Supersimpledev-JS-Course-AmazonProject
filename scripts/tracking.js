70984956b39c88c15ca95a4b31daf1c824fc890c
PaoloBaltazar <gpaolobaltazar@gmail.com>
2024-06-29 21:48:55 +0800

    Make tracking page interactive

--- scripts/tracking.js
+++ scripts/tracking.js
@@ -18,6 +18,13 @@ async function loadPage() {
     }
   });
 
+  const today = dayjs();
+  const orderTime = dayjs(order.orderTime);
+  const deliveryTime = dayjs(productDetails.estimatedDelivaryTime);
+  const percentProgress = ((today - orderTime) / (deliveryTime - orderTime)) * 100;
+
+  console.log(percentProgress);
+
   const trackingHTML = `
     <a class="back-to-orders-link link-primary" href="orders.html">
       View all orders
@@ -38,19 +45,19 @@ async function loadPage() {
     <img class="product-image" src="${product.image}">
 
     <div class="progress-labels-container">
-      <div class="progress-label">
+      <div class="progress-label ${percentProgress < 50 ? 'current-status' : ''}">
         Preparing
       </div>
-      <div class="progress-label current-status">
+      <div class="progress-label ${(percentProgress >= 500 && percentProgress < 100) ? 'current-status' : ''}">
         Shipped
       </div>
-      <div class="progress-label">
+      <div class="progress-label ${percentProgress >= 100 ? "current-status" : ''}">
         Delivered
       </div>
     </div>
 
     <div class="progress-bar-container">
-      <div class="progress-bar"></div>
+      <div class="progress-bar" style="width: ${percentProgress}%"></div>
     </div>
   `;
 
