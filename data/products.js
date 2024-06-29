59f07f6aad761ba48971f341876625cb1be77552
PaoloBaltazar <gpaolobaltazar@gmail.com>
2024-06-29 10:59:05 +0800

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -91,6 +91,7 @@ export function loadProductsFetch() {
     'https://supersimplebackend.dev/products'
   ).then((response) => {
     return response.json(); //This returns a promise that needs to be load first before proceeding to the next step
+
   }).then((productsData) => {
     products = productsData.map((productDetails) => {
       if (productDetails.type === 'clothing') {
@@ -100,10 +101,13 @@ export function loadProductsFetch() {
     });
 
     console.log('load products');
-  });  
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
 
   return promise;
 }
+
 
 // loadProductsFetch().then(() => {
 //   console.log('next step');
@@ -125,9 +129,14 @@ export function loadProducts(fun) {
     fun();
   });
 
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.')
+  });
+
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
+
 
 // export const products = [
 //   {
